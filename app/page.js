35f2a53f82ca17b50348d6f83dc006aab9092404
Ross Williams <rossjrw@gmail.com>
2026-01-14 10:39:09 +0000

    Add layout for nextjs 15 compatibility

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
 import './styles.css'
 
 export default function Page() {
-  return (
-    <div>
-      <h1 className="direct-test">Direct Layer Test</h1>
-      <p className="nested-test">Nested Layer Test</p>
-      <span className="external-test">External Layer Test</span>
-    </div>
-  )
+  return <div></div>
 }
